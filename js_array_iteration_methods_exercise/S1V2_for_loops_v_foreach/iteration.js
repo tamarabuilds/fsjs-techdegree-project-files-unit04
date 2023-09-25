8d1d72f8616c63af98cc567eb5289409047a7d7c
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-25 10:33:08 -0700

    forEach array training

--- js_array_iteration_methods_exercise/S1V2_for_loops_v_foreach/iteration.js
+++ js_array_iteration_methods_exercise/S1V2_for_loops_v_foreach/iteration.js
@@ -1,1 +1,26 @@
-const fruits = ['apple', 'pear', 'cherry'];
+// const fruits = ['apple', 'pear', 'cherry'];
+
+// const capitalizedFruits = [];
+
+// fruits.forEach( fruit => capitalizedFruits.push(fruit.toUpperCase()))
+// console.log(capitalizedFruits)
+
+// const prices = [6.75, 3.10, 4.00, 8.12];
+
+//     // Total: 21.97
+
+// let total = 0;
+
+// prices.forEach( price => total += price)
+
+// console.log(total);
+
+
+const names = ['Selma', 'Ted', 'Mike', 'Sam', 'Sharon', 'Marvin'];
+
+    // Result: ['Selma', 'Sam', 'Sharon'];
+
+let sNames = [];
+
+names.forEach( (name,i) => console.log(`${i +1}: ${name}`));
+
